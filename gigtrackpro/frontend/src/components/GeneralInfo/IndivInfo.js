09d692011fb363eb9f7423f2271f072c6d0841dc
bvchnguyen <bang4509@colorado.edu>
2023-07-20 18:26:33 -0700

    Implemented trip info under the general info

---
+++ gigtrackpro/frontend/src/components/GeneralInfo/IndivInfo.js
@@ -0,0 +1,133 @@
+import React from "react";
+import styled from 'styled-components';
+import { useGlobalContext } from "../../context/Global";
+import  { car, clock, gas } from "../../utils/Icons";
+import EarningsModal from "../Modal/EarningsModal";
+import EarningsItems from "../EarningsItems/EarningsItems";
+
+function IndivInfo (){
+
+    const { totalEarnings, getAverageTripRatio } = useGlobalContext();
+
+
+    return (
+        <IndivInfoStyled>
+        <div className="avgstats-content">
+        <div className="indi-content">
+                <div className="icon">
+                    {car}
+                </div>
+                <div className="inner-content">
+                    <div className="text">
+                        <p>Dollars per mi</p>
+                        <h2>${getAverageTripRatio()}</h2>
+                    </div>
+                </div>
+                <div className="miles-rating">
+                    <h6>Good</h6>
+                </div>
+            </div>
+            <div className="indi-content">
+                <div className="icon">
+                    {clock}
+                </div>
+                <div className="inner-content">
+                    <div className="text">
+                        <p>Hours Online</p>
+                        <h2>25h</h2>
+                    </div>
+                </div>
+                <div className="hours-rating">
+                    <h6>Fair</h6>
+                </div>
+            </div>
+            <div className="indi-content">
+                <div className="icon">
+                    {gas}
+                </div>
+                <div className="inner-content">
+                    <div className="text">
+                        <p>Fuel</p>
+                        <h2>$92.68</h2>
+                    </div>
+                </div>
+                <div className="fuel-rating">
+                    <h6>low</h6>
+                </div>
+            </div>
+            </div>
+        </IndivInfoStyled>
+    )
+
+}
+
+const IndivInfoStyled = styled.div`
+    width: 100%;
+    margin-top: 1rem;
+    /* height: 10vh; */
+    .avgstats-content{
+        display: flex;
+        flex-direction: row;
+        gap: 1rem;
+        /* background-color: #2c2c2c; */
+        .indi-content{
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            padding: 1rem;
+            background-color: white;
+            border-radius: 15px;
+            height: 70px;
+            width: 100%;
+            gap: 10px;
+            .icon {
+                width: 45px;
+                height: 45px;
+                background-color: #EAEAEA;
+                border-radius: 50%;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                font-size: 20px;
+            }
+            .inner-content{
+                display: flex;
+                flex-direction: column;
+                .text{
+                    width: 100%;
+                    margin-right: 30px;
+                    p{
+                        font-size: 12px;
+                    }
+                    h2{
+                        color: #2c2c2c;
+                        font-size: 17px; 
+                    }
+                }
+            }
+            .miles-rating, .hours-rating, .fuel-rating{
+                display: flex;
+                width: 35px;
+                height: 20px;
+                align-items: center;
+                justify-content: center;
+                border-radius: 5px;
+                /* background-color: #3FC060; */
+                padding: 2px;
+                h6{
+                    color: white;
+                    font-size: 10px;
+                }
+            }
+            .miles-rating, .fuel-rating{
+                background-color: #3FC060;
+            }
+            .hours-rating{
+                background-color: #FFA94C;
+            }
+        }
+        
+    }
+`;
+
+export default IndivInfo;
--- gigtrackpro/frontend/src/components/Statistics/Statistics.js
+++ gigtrackpro/frontend/src/components/Statistics/Statistics.js
@@ -3,24 +3,72 @@ import styled from 'styled-components'
 import { useGlobalContext } from "../../context/Global";
 import { Innerlayout } from "../../styles/Layouts";
 import EarningsGoals from "../Goals/Goals";
+import GenInfo from "../GeneralInfo/GenInfo";
+import StatsChart from "../Chart/Chart";
+import IndivInfo from "../GeneralInfo/IndivInfo";
 
 function Statistics () {
 
-    const { totalEarnings } = useGlobalContext();
+    const { totalEarnings, getAverageTripRatio } = useGlobalContext();
     const goal = 2200; // Set the goal value here
     const goalDifference = goal - totalEarnings();
     return (
         <StatisticsStyled>
             <Innerlayout>
-                <EarningsGoals goal={goal} earnings={goalDifference} />
+                {/* <EarningsGoals goal={goal} earnings={goalDifference} /> */}
+                <div className="statistics-content">
+                <GenInfo />
+                <IndivInfo />
+                <div className="chart-container">
+                        <StatsChart />
+                    <div className="weekly-stats">
+                    </div>
+                </div>
+               </div>
             </Innerlayout>
         </StatisticsStyled>
     )
 }
 
 const StatisticsStyled = styled.div `
-
-
+    .statistics-content{
+        width: 100%;
+        padding: 2rem;
+        padding-top: 0;
+        /* gap: 1rem; */
+    }
+    .chart-container{
+        width: 100%;
+        display: flex; 
+        flex-direction: row;
+        justify-content: flex-start;
+        /* background-color: blue; */
+        height: 50vh;
+        align-items: center;
+        /* padding-top: 2rem; */
+        gap: 1rem;
+        /* padding-right: 1rem; */
+        /* margin-right: 10px; */
+        align-items: center;
+    }
+    /* .avgstats-content{
+        display: flex;
+        flex-direction: column;
+        .avgtrip-ratio{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: space-around;
+            padding: 1rem;
+            background-color: white;
+            border-radius: 20px;
+            height: 5rem;
+            width: 100%;
+            p{
+                font-size: 10px;
+            }
+        }
+    } */
 `;
 
 export default Statistics;
