b3b37018dc20653b4509eb089215669f4fa34c32
bvchnguyen <bang4509@colorado.edu>
2023-07-26 14:27:42 -0600

    Redesigned expense schema, added expense log button and form

---
+++ gigtrackpro/frontend/src/components/Form/ExpenseForm.jsx
@@ -0,0 +1,141 @@
+import React, { useState } from "react";
+import styled from 'styled-components';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useGlobalContext } from "../../context/Global";
+
+function ExpenseForm ({ toggleModal }) {
+    
+    const {addExpense, getExpense} = useGlobalContext()
+    const [inputState, setInputState] = useState({
+        amount: '',
+        date: '',
+        category: '',
+    })
+    const { amount, date, category } = inputState; 
+
+    const handleInput = name => e => {
+        setInputState({...inputState, [name]: e.target.value})
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        // addEarnings(inputState)
+        addExpense(inputState)
+        getExpense()
+        toggleModal()
+        // getEarnings()
+    }
+    return (
+        <ExpenseFormStyled onSubmit={handleSubmit}>
+            <div className="input-control">
+                <input
+                    className={'input-highlight'}
+                    autoComplete="off"
+                    type="text"
+                    value={amount}
+                    name={'amount'}
+                    placeholder="Expense Amount"
+                    onChange={handleInput('amount')}
+                />
+            </div>
+            <div className="input-control">
+                <DatePicker
+                    className={'input-highlight'}
+                    autoComplete="off"
+                    id='date'
+                    placeholderText="Enter Date"
+                    selected={date}
+                    dateFormat="MM/dd/yyyy"
+                    onChange={(date) => {
+                        setInputState({...inputState, date: date})
+                    }}
+                />
+            </div>
+            <div className="select-input-control">
+                <select required value = {category} name="category" id="category" onChange={handleInput('category')}>
+                    <option value="" disabled id="d-select">Select Expense</option>
+                    <option value="fuel" >Fuel</option>
+                    <option value="maintenance" >Maintenance</option>
+                    <option value="repairs"  >Repairs</option>
+                </select>
+            </div>
+            <div className="submit-btn">
+                <button className="log-button">Log Expense</button>
+            </div>
+        </ExpenseFormStyled>
+    )
+}
+
+const ExpenseFormStyled = styled.form`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    input, textarea, select{
+        height: 40px;
+        /* font-family: 'Roboto', sans-serif; */
+        font-size: 1rem;
+        font-family: inherit;
+        font-weight: none;
+        outline: none;
+        border: 1px solid #D0D0D0;
+        border-radius: 6px;
+        padding: .5rem 1rem;
+        background: transparent;
+        resize: none;
+        &::placeholder{
+            font-size: 15px;
+            color: #B2B2B2;
+        }
+    }
+    .input-control{
+        input{
+            width: 100%;
+        }
+    }
+    .input-control input.input-highlight {
+        border: 1px solid #D0D0D0;
+    }
+
+    .input-control input.input-highlight:focus {
+        border-color:  #3FC060;
+        box-shadow: 0 0 1px 1px #ABE4BA;
+        outline: none;
+    }
+    select{
+        width: 100%;
+    }
+    #date{
+            width: 320px;
+    }
+    .log-button{
+        margin-top: 10px;
+        background-color: #3FC060;
+        color: white;
+        cursor: pointer;
+        font-size: 17px;
+        font-weight: bold;
+        display: grid;
+        place-items: center;
+        text-align: center;
+        border: none;
+        /* border: 1px solid #565656; */
+        height: 50px;
+        width: 100%;
+        place-items: center;
+        border-radius: 7px;
+        text-decoration: none;
+        letter-spacing: 2px;
+        box-shadow: inset 0 0 0 0 #FF4B38;
+    }
+    .box-title{
+        font-weight: 200;
+        margin-bottom: .5rem;
+    }
+    .box-title-app{
+        font-weight: 200;
+    }
+
+`;
+
+export default ExpenseForm;
---
+++ gigtrackpro/frontend/src/components/Modal/ExpenseModal.jsx
@@ -0,0 +1,121 @@
+import React, { useState, useEffect } from "react";
+import Form from "../Form/Form";
+import ExpenseForm from "../Form/ExpenseForm"
+import styled from 'styled-components';
+
+function ExpenseModal (){
+    const [modal, setModal] = useState(false);
+    
+    const toggleModal = () => {
+        setModal(!modal)
+    }
+    useEffect(() => {
+        if (modal) {
+          document.body.classList.add('active-modl');
+        } 
+        else {
+          document.body.classList.remove('active-modl');
+        }
+    }, [modal]);
+
+    return (
+        <ExpenseModalStyled>
+            <button onClick={toggleModal} className="btn-modal">
+                <h3>Log Expense</h3>
+            </button>
+            {modal && (
+                <div className="modal">
+                    <div className="overlay" onClick={toggleModal}></div>
+                    <div className="modal-content">
+                        <h1 className="modal-heading">Log Expense</h1>
+                        <ExpenseForm toggleModal= {toggleModal} />
+                        <button 
+                            className="close-modal" 
+                            onClick={toggleModal}
+                        >X
+                        </button>
+                    </div>
+                </div>
+            )}
+        </ExpenseModalStyled>
+    )
+}
+
+const ExpenseModalStyled = styled.nav`
+
+    .modal-heading {
+        color: #2c2c2c;
+        text-align: center;
+        margin-top: 0;
+        margin-bottom: 1rem;
+    }
+    body.active-modal {
+        overflow-y: hidden;
+    }
+
+    .btn-modal {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        margin-top: 10px;
+        background-color: #3FC060;
+        color: white;
+        cursor: pointer;
+        place-items: center;
+        text-align: center;
+        border: none;
+        height: 30px;
+        width: 120px;
+        border-radius: 7px;
+        letter-spacing: 2px;
+        h3{
+            margin-left: 5px;
+            font-size: 15px;
+            font-weight: 100;
+        } 
+    }
+    .modal{
+        z-index: 9999;
+    }
+    .modal, .overlay {
+        width: 100vw;
+        height: 100vh;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        position: fixed;
+    }
+
+    .overlay {
+        background: rgba(49,49,49,0.8);
+    }
+    .modal-content {
+        position: absolute;
+        top: 40%;
+        left: 50%;
+        width: 22rem;
+        transform: translate(-50%, -50%);
+        line-height: 1.4;
+        background: #f1f1f1;
+        padding: 1rem 1rem;
+        border-radius: 10px;
+        max-width: 600px;
+        min-width: 300px;
+    }
+
+    .close-modal {
+        cursor: pointer;
+        background-color: none;
+        color: #7D7D7D;
+        font-size: 15px;
+        border: none;
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        padding: 5px 7px;
+    }
+`;
+
+export default ExpenseModal;
